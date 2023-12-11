9b4fcefb6252f9d52ef229b94bfb81f5aff360af
DiasNabil <donrogue@protonmail.com>
2023-12-11 14:24:33 +0300

    login - register -dashboard UI

--- frontend/core-front/.next/static/chunks/webpack.js
+++ frontend/core-front/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c710905b810913fe"; }
+/******/ 		__webpack_require__.h = function() { return "5a02b32135f76a3b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
